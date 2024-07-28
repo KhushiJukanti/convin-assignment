947da3a3f6794534a488582201f441faeb0f536c
IK <khushijukanti33@gmail.com>
2024-07-28 22:06:13 +0530

    Deleted .env file

--- models/expense.js
+++ models/expense.js
@@ -5,8 +5,8 @@ const expenseSchema = new mongoose.Schema({
   amount: { type: Number, required: true },
   splitMethod: { type: String, enum: ['equal', 'exact', 'percentage'], required: true },
   splits: { type: Map, of: Number, required: true },
-  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'UserList', required: true },
+  participants: [{ type: mongoose.Schema.Types.ObjectId, ref: 'UserList' }],
 });
 
 const Expense = mongoose.model('Expense', expenseSchema);
